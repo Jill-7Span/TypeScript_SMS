fbdab739f109f5d518e34b5d996aea3fd8ccc4ec
Jill-7span <113359267+Jill-7span@users.noreply.github.com>
2023-03-01 11:38:42 +0530

    change in tag module

--- tag/tag.ts
+++ tag/tag.ts
@@ -1,24 +1,25 @@
 import express from 'express';
 import { Auth } from '../middleware/authMiddleware';
-import tagController from './tagController';
+import { TagController } from './tagController';
 
 export class Tag {
   public router = express.Router();
-  public auth : Auth;
+  public auth: Auth;
+  public tagController: TagController;
 
-  constructor () {
+  constructor() {
     this.auth = new Auth();
+    this.tagController = new TagController();
   }
 
   public inRoute() {
-    this.router.get('/findTag', this.auth.authOfBusiness, tagController.findTags);
+    this.router.get('/findTag', this.auth.authOfBusiness, this.tagController.findTags);
 
-    this.router.get('/allTag', this.auth.authOfBusiness, tagController.allTags);
+    this.router.get('/allTag', this.auth.authOfBusiness, this.tagController.allTags);
 
-    this.router.post('/createTag', this.auth.authOfBusiness, tagController.createTag);
+    this.router.post('/createTag', this.auth.authOfBusiness, this.tagController.createTag);
 
-    this.router.delete('/deleteTag', this.auth.authOfBusiness, tagController.deleteTag);
+    this.router.delete('/deleteTag', this.auth.authOfBusiness, this.tagController.deleteTag);
   }
 }
 
-module.exports = router;
